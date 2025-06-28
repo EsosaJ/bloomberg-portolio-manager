3073ed772c80d7179be568c1caaed470b05f3ed1
EsosaJ <esosajohnny360@gmail.com>
2025-06-28 08:46:59 +0100

    Updated test security class

--- PortfolioManager/0-Securities/test_security.py
+++ PortfolioManager/0-Securities/test_security.py
@@ -10,7 +10,7 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License.
+# limitatdions under the License.
 
 import os
 import sys
@@ -23,9 +23,15 @@ import implementations.securitySolution
 def test_securityInit():
     #GIVEN
     EXPECTED_NAME = "DSAQ US Equity"
+    EXPECTED_STRING = "Security: DSAQ US Equity"
 
     #WHEN
     testObj = implementations.securitySolution.security(EXPECTED_NAME)
 
+
     #EXPECT
     assert (testObj.getName() == EXPECTED_NAME)
+    assert str(testObj) == EXPECTED_STRING
+    print("🤏🏾Test worked!")
+
+
